ef72fd3a57f1b0487d29db7f96cb63f184fb2985
johnny-lighthouse <git.x.jjl@9ox.net>
2012-02-04 21:45:01 -0500

    bring ldif into if then.  thoughts on how to handle group creation

--- mkldif.pl
+++ mkldif.pl
@@ -23,36 +23,39 @@ while (<DATAFILE>) {
 	my $group;
 
 	# for each line find username, email, and group
-	if ($_ =~ /(\S+)\s+=>\s+(\S+@\S+)\s+:B:\w+:\w*\s+(\w+)/i)
+	if ($_ =~ /(\S+)\s+=>\s+(\S+@\S+)\s+:B:\w+:\w*\s+(\w+)/i) {
 
-		{
+		#fill variables from pattern match
 		$username = $1;
 		$email = $2;
-		$group = $3
+		$group = $3;
 
-		}
+		#print variables to verify correct operation
+		print "Set username= " . $username . " Set email= " . $email . " Set group= " . $group . "\n";
 
-	#print variables to verify correct operation
-	print "Set username= " . $username . " Set email= " . $email . " Set group= " . $group . "\n\n";
+		# create ldif for user
+		print "dn: uid=" . $username . ",ou=x,dc=y,dc=z,dc=com\n";
+		print "uid: " . $username . "\n";
+		print "sn: " . $username . "\n"; 
+		print "objectClass: top\n";
+		print "objectClass: person\n";
+		print "objectClass: inetOrgPerson\n";
+		print "mail: " . $email . "\n\n";
 
-	# create ldif for user
-	print "dn: uid=" . $username . ",ou=x,dc=y,dc=z,dc=com\n";
-	print "uid: " . $username . "\n";
-	print "sn: " . $username . "\n"; 
-	print "objectClass: top\n";
-	print "objectClass: person\n";
-	print "objectClass: inetOrgPerson\n";
-	print "mail: " . $email . "\n\n";
+		# create group if not seen before
+			#can we check to see if $group exists in an interloop persistent data structure (hash, array, etc)
+			#if not then add it
+		# add user to group if it exists
+			#we really want to create group all at once so the memebrs are grouped together in the ldap output
+			#can we store dn and group in a persistent structure and create groups after processing all lines?
+			#can i use one structure for both?  there an easy way to list unique values in a multi fielded structure?
 
-	# create group if not seen before
-
-	# add user to group if it exists
+	   }
 
 	#indicate completion of loop
-#	print "\n\n";
+	#print "\n\n";
 
     }
-
 
 #close open files and any other cleanup
 close (DATAFILE);
