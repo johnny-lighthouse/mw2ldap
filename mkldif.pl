c8ba831532ddb6cff54b24d5665888a6a00a7ca9
johnny-lighthouse <git.x.jjl@9ox.net>
2012-02-04 21:22:29 -0500

    cleanup. declare variables inside loop, remove chomp and extra newline, format comments, juggle newlines in print statements.

--- mkldif.pl
+++ mkldif.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 #
-# create ldif file from file containing user info
+# create ldif from file containing user info
 # we are expecting one record per line with whitespace delimited fields
 # we are expecting 'username => email password-hash groupname'
 # output will print to standard out and can be piped somewhere else
@@ -9,26 +9,20 @@ use strict;
 use warnings;
 
 # open file containing user info
-
-open (DATAFILE, 'pretty_combined');
-
-#setup variables for use in loop
-my $username;
-my $email;
-my $group;
+open (DATAFILE, 'user-data.txt');
 
 # loop through by line
-
 while (<DATAFILE>) {
 
-	#clear off newline at end of line string
-	chomp;
+	# print the whole line to demonstrate proper read
+	print "$_";
 
-	#print the whole line to demonstrate proper read
-	print "$_\n";
+	# initialize variables
+	my $username;
+	my $email;
+	my $group;
 
 	# for each line find username, email, and group
-	
 	if ($_ =~ /(\S+)\s+=>\s+(\S+@\S+)\s+:B:\w+:\w*\s+(\w+)/i)
 
 		{
@@ -39,30 +33,26 @@ while (<DATAFILE>) {
 		}
 
 	#print variables to verify correct operation
-	print "Set username= " . $username . " Set email= " . $email . " Set group= " . $group . "\n";
+	print "Set username= " . $username . " Set email= " . $email . " Set group= " . $group . "\n\n";
 
 	# create ldif for user
-
-	print "\n";
-
 	print "dn: uid=" . $username . ",ou=x,dc=y,dc=z,dc=com\n";
 	print "uid: " . $username . "\n";
 	print "sn: " . $username . "\n"; 
 	print "objectClass: top\n";
 	print "objectClass: person\n";
 	print "objectClass: inetOrgPerson\n";
-	print "mail: " . $email . "\n";
+	print "mail: " . $email . "\n\n";
 
 	# create group if not seen before
 
 	# add user to group if it exists
 
 	#indicate completion of loop
-	print "\n\n";
+#	print "\n\n";
 
-	}
+    }
 
 
 #close open files and any other cleanup
-
 close (DATAFILE);
